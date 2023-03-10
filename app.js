d666a466654c9d533d9634735e65ba08b36bf23b
samir  jose hadechni bonett <samirjhb@MacBook-Pro-de-samir.local>
2023-03-09 21:18:30 -0300

    Manejo de errores en slack

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-require("dotenv").config()
-const  express = require('express')
-const cors = require("cors")
-const dbConnect = require("./config/mongo") 
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dbConnect = require("./config/mongo");
+const app = express();
+const morganBody = require("morgan-body");
+const loggerStream = require("./utils/handleLogger");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("storage"));
 
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static("storage"))
+morganBody(app, {
+  noColors: true,
+  stream: loggerStream,
+  skip: function (req, res) {
+    return res.statusCode < 400;
+  },
+});
 
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT || 3000;
 
 /**
  * Aqui invocamos a las rutas!👨‍💻👨‍💻
  */
 
-app.use("/api", require("./routes"))
+app.use("/api", require("./routes"));
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`App listening on port ${port}!🛸🛸`))
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`App listening on port ${port}!🛸🛸`));
 
-dbConnect()
+dbConnect();
